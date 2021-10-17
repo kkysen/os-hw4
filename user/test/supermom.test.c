5a1327c10b3980779db139f6d868d3a070fe2478
Khyber Sen <kkysen@gmail.com>
2021-10-17 05:07:55 -0400

    I realized I can't run `supermom.test` as root (using `sudo`) and then downgrade, since uid is 0, not just euid. Also, I can't `execvp` `sudo` from `supermom.test` since `sudo` doesn't let you do that. So I just run `supermom.test` twice now, once normally and once with `sudo`.

--- user/test/supermom.test.c
+++ user/test/supermom.test.c
@@ -9,23 +9,13 @@
 #include "supermom.h"
 
 void check(pid_t pid, uid_t *uid, Check *checker, SourceLocation location,
-	   int error, const char *message)
+	   int error, FILE *output, const char *message)
 {
 	supermom(pid, uid);
 	Check_errno(checker, location, error);
 	if (message) {
-		fprintf(stdout, "%s\n", message);
-		fflush(stdout); // need to flush or we flush multiple times when exiting from the forked child
-	}
-}
-
-void run_as_root_or_not(bool as_root)
-{
-	bool is_root = geteuid() == 0;
-	if (as_root) {
-		assert(is_root);
-	} else {
-		assert(seteuid(getuid()) != -1);
+		fprintf(output, "%s\n", message);
+		fflush(output); // need to flush or we flush multiple times when exiting from the forked child
 	}
 }
 
@@ -42,42 +32,35 @@ static void uid_print(FILE *output, const void *uid_void)
 	fprintf(output, "%d", *uid);
 }
 
-void test(Check *checker, bool as_root)
-{
-	uid_t euid_expected = geteuid();
-
-	const pid_t pid = fork();
-	assert(pid != -1);
-	if (pid != 0) {
-		// parent
-		wait(NULL);
-		return;
-	}
-
-	// child
-	run_as_root_or_not(as_root);
-	int error = as_root ? 0 : EACCES;
-
-	check(getppid(), NULL, checker, HERE, error, NULL);
-
-	uid_t euid_actual = -1;
-	check(getppid(), &euid_actual, checker, HERE, error, NULL);
-	Check_eq(checker, HERE, &euid_actual, &euid_expected, uid_eq, uid_print,
-		 "euid");
-	exit(EXIT_SUCCESS);
-}
-
-/// stdout should be what's printed in the kernel log.
+/// stdout or fd 3 (if open) should be what's printed in the kernel log.
 int main()
 {
 	setlinebuf(stderr);
 	Check checker = Check_new(ColorOutput_default(stderr));
+	FILE *output = fdopen(3, "w");
+	if (!output) {
+		errno_reset();
+		output = stdout;
+	}
 
-	check(getpid(), NULL, &checker, HERE, EINVAL, "Not Yo Mama");
-	check(1 /* init */, NULL, &checker, HERE, EINVAL, "Not Yo Mama");
+	const uid_t euid = geteuid();
+	bool is_root = euid == 0;
 
-	test(&checker, true);
-	test(&checker, false);
+	check(getpid(), NULL, &checker, HERE, EINVAL, output, "Not Yo Mama");
+	check(1 /* init */, NULL, &checker, HERE, EINVAL, output,
+	      "Not Yo Mama");
 
+	int error = is_root ? 0 : EACCES;
+
+	check(getppid(), NULL, &checker, HERE, error, output, NULL);
+
+	uid_t supermom_euid = -1;
+	check(getppid(), &supermom_euid, &checker, HERE, error, output, NULL);
+	Check_eq(&checker, HERE, &supermom_euid, &euid, uid_eq, uid_print,
+		 "euid");
+
+	if (output != stdout) {
+		fclose(output);
+	}
 	Check_exit(&checker);
 }
