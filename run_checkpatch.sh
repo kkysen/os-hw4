a3bcc4e7b95f4bbaa7aceee5d972ed0541cdb983
Khyber Sen <kkysen@gmail.com>
2021-10-17 22:02:24 -0400

    Updated check patch to run in `user/module/`, too, since that's still kernel mode code.

--- run_checkpatch.sh
+++ run_checkpatch.sh
@@ -4,7 +4,7 @@ INIT_COMMIT=$(git log --reverse --pretty=format:"%h" | head -n 1)
 IGNORES=FILE_PATH_CHANGES,SPDX_LICENSE_TAG
 
 HW4_IGNORES=$IGNORES,EXPORT_SYMBOL,ENOSYS,AVOID_EXTERNS
-git diff $INIT_COMMIT linux/ | linux/scripts/checkpatch.pl --ignore $HW4_IGNORES
+git diff $INIT_COMMIT linux/ user/module/ | linux/scripts/checkpatch.pl --ignore $HW4_IGNORES
 
 # ENOSYS: Looks like ENOSYS is often misused in the kernel so checkpatch always
 #         warns on seeing ENOSYS. Our use is correct, though!
